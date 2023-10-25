e583c2de57b80e05dd33492620fb2d07c6cba6e6
Helbertt Dalarme <hdalarme@yahoo.com.br>
2023-10-25 17:07:55 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using fundamentos.Models;
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Helbertt";
+pessoa1.Idade = 38;
+
+pessoa1.Apresentar();
